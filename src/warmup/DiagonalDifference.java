66fb71334620e19cbeb48d5b6ff29286d24e89d3
kimsubin <kimddub@gmail.com>
2022-12-13 22:54:46 +0900

    feat: 추가 DiagonalDifference 풀이
    - Add Warmup/DiagonalDifference.java
    - Edit README.md

---
+++ src/warmup/DiagonalDifference.java
@@ -0,0 +1,68 @@
+package warmup;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+import static java.util.stream.Collectors.toList;
+
+public class DiagonalDifference {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new OutputStreamWriter(System.out));
+//        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        int n = Integer.parseInt(bufferedReader.readLine().trim());
+
+        List<List<Integer>> arr = new ArrayList<>();
+
+        IntStream.range(0, n).forEach(i -> {
+            try {
+                arr.add(
+                        Stream.of(bufferedReader.readLine().replaceAll("\\s+$", "").split(" "))
+                                .map(Integer::parseInt)
+                                .collect(toList())
+                );
+            } catch (IOException ex) {
+                throw new RuntimeException(ex);
+            }
+        });
+
+        int result = DiagonalDifferenceResult.diagonalDifference(arr);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
+
+
+class DiagonalDifferenceResult {
+
+    /*
+     * Complete the 'diagonalDifference' function below.
+     *
+     * The function is expected to return an INTEGER.
+     * The function accepts 2D_INTEGER_ARRAY arr as parameter.
+     */
+
+    public static int diagonalDifference(List<List<Integer>> arr) {
+        int n = arr.get(0).size();
+        int m = arr.size();
+
+        int leftToRightSum = 0;
+        int rightToLeftSum = 0;
+
+        for (int i=0; i<m; i++) {
+            leftToRightSum += arr.get(i).get(i);
+            rightToLeftSum += arr.get(i).get(n - 1 - i);
+        }
+
+        return Math.abs(leftToRightSum - rightToLeftSum);
+    }
+
+}
